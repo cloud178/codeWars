da0d184e4bdb2d7c11c18a39aadb3291e7df99c3
cloud178 <denis-biryukov-13@mail.ru>
2024-09-29 22:01:02 +0300

    another one task

---
+++ kyu6/StopGninnipsMySdroW.js
@@ -0,0 +1,23 @@
+// Write a function that takes in a string of one or more words, and returns the same string, but with
+// all words that have five or more letters reversed (Just like the name of this Kata). Strings passed in will
+// consist of only letters and spaces. Spaces will be included only when more than one word is present.
+//
+//     Examples:
+//
+// "Hey fellow warriors"  --> "Hey wollef sroirraw"
+// "This is a test        --> "This is a test"
+// "This is another test" --> "This is rehtona test"
+
+function spinWords(string){
+    const arr = string.split(' ');
+    let result_arr = [];
+    for (elem of arr) {
+        if (elem.length > 4) {
+            elem = elem.split('').reverse().join('');
+            result_arr.push(elem);
+            continue;
+        }
+        result_arr.push(elem);
+    }
+    return result_arr.join(' ');
+}
